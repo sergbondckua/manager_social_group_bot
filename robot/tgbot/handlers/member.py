f3d785a646d7b9ed3daa4214d6b84ef6fc201495
sergbondckua <sergninja@gmail.com>
2025-03-22 14:51:08 +0200

    fix logic

--- robot/tgbot/handlers/member.py
+++ robot/tgbot/handlers/member.py
@@ -16,19 +16,15 @@ member_router.message.filter(ClubMemberFilter())
 # Об'єднаний обробник команди /start
 @member_router.message(CommandStart())
 async def handle_start(message: types.Message, command: types.BotCommand):
-    """ Обробка команди /start з підтримкою deep link. """
+    """Обробка команди /start з підтримкою deep link."""
 
     # Отримуємо аргумент deep link з команди
     deep_link_param = command.args
 
     # Отримуємо фото користувача
 
-    photos = await message.bot.get_user_profile_photos(
-        message.from_user.id
-    )
-    photo_id = (
-        photos.photos[0][-1].file_id if photos.total_count > 0 else None
-    )
+    photos = await message.bot.get_user_profile_photos(message.from_user.id)
+    photo_id = photos.photos[0][-1].file_id if photos.total_count > 0 else None
 
     # Дані користувача
     user_data = {
@@ -50,7 +46,18 @@ async def handle_start(message: types.Message, command: types.BotCommand):
         # Оновлюємо інформацію про користувача, якщо він вже існує
         for field, value in user_data.items():
             setattr(user, field, value)
-        await user.asave()  # Асинхронне збереження
+        await user.asave()
+
+    is_full_data = all([user.data_of_birth, user.phone_number])
+
+    if is_full_data and not deep_link_param:
+        await message.answer(
+            f"Вітаю з поверненням, {message.from_user.first_name}!"
+        )
+    elif not is_full_data:
+        await message.answer(
+            msg_handle_start.format(name=message.from_user.first_name)
+        )
 
     # Якщо deep link параметр вказаний
     if deep_link_param:
@@ -87,11 +94,3 @@ async def handle_start(message: types.Message, command: types.BotCommand):
             await message.answer(
                 "Сталася помилка у форматуванні повідомлення."
             )
-    else:
-        # Якщо deep link параметр відсутній (звичайний старт)
-        msg = (
-            f"Вітаю з поверненням, {message.from_user.first_name}!"
-            if not created
-            else msg_handle_start.format(name=message.from_user.first_name)
-        )
-        await message.answer(msg)
